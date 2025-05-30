8cba8970be0ca6622246a2b52733a0a2a4f168bf
PavelProjects <pavelprojects52@gmail.com>
2025-05-30 18:36:31 +0300

    add env load

--- vite.config.js
+++ vite.config.js
@@ -5,30 +5,34 @@ import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+  
+  return {
+    plugins: [
+      vue(),
+      vueDevTools(),
+    ],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      },
     },
-  },
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:8080/',
+    server: {
+      proxy: {
+        '/api': {
+          target: 'http://localhost:8080/',
+        }
       }
-    }
-  },
-  build: {
-    rollupOptions: {
-      output: {
-        entryFileNames: 'assets/script.js',
-        assetFileNames: 'assets/styles.css'
+    },
+    build: {
+      rollupOptions: {
+        output: {
+          entryFileNames: 'assets/script.js',
+          assetFileNames: 'assets/styles.css'
+        }
       }
-    }
-  },
-  base: env.BASE_PATH ?? ''
+    },
+    base: env.BASE_PATH ?? ''
+  }
 })
