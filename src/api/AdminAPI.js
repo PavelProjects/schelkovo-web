d3022444c991cfb9cb9599b1818c588a56be4e68
PavelProjects <pavelprojects52@gmail.com>
2025-05-30 18:45:37 +0300

    baseURL fix

--- src/api/AdminAPI.js
+++ src/api/AdminAPI.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 const { pathname } = window.location
 const axiosConfig = {
-  baseUrl: pathname,
+  baseURL: pathname,
   timeout: 5000,
   withCredentials: true
 }
--- src/api/BackendAPI.js
+++ src/api/BackendAPI.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 const { pathname } = window.location
 const axiosConfig = {
-  baseUrl: pathname,
+  baseURL: pathname,
   timeout: 5000,
 }
 const publicAxios = axios.create(axiosConfig)
